794b1ef47b45a30370b846d5761010c93b7cc8cc
Aron Greenj <arongreen0501@gmail.com>
2017-02-13 10:48:35 +0000

    Adding header

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,11 +5,13 @@ import {
   Text,
   View
 } from 'react-native';
+import Header from './src/components/Header';
 
 export default class repeat extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Header headerText={'App'} />
         <Text>FLEH</Text>
       </View>
     );
